69abd48b3824c7964d1c50d74383f93a798b28ca
Uziel Bueno <UBueno@grupohi.mx>
2014-05-28 10:52:23 -0500

    -- Version final del modulo

--- inc/lib/db/db_config.php
+++ inc/lib/db/db_config.php
@@ -1,28 +1,22 @@
 <?php
 //Datos de configuración de la conexión a la base de datos
-$db_host['ModulosSAO'] = '(local)\SQL2012';
+$db_host['ModulosSAO'] = '(local)';
 $db_user['ModulosSAO'] = 'App_ModulosSAO';
 $db_pwd['ModulosSAO']  = '@msApp85%';
 $db_name['ModulosSAO'] = 'ModulosSAO';
 $db_type['ModulosSAO'] = 'MSAO';
 
-$db_host['ReportesSAO'] = '(local)\SQL2012';
-$db_user['ReportesSAO'] = 'App_ModulosSAO';
-$db_pwd['ReportesSAO']  = '@msApp85%';
-$db_name['ReportesSAO'] = 'ReportesSAO';
-$db_type['ReportesSAO']  = 'RSAO';
-
-$db_host['SAO1814'] = '(local)\SQL2012';
+$db_host['SAO1814'] = '(local)';
 $db_user['SAO1814'] = 'App_ModulosSAO';
 $db_pwd['SAO1814']  = '@msApp85%';
 $db_name['SAO1814'] = 'SAO1814';
 $db_type['SAO1814'] = 'OBRA';
 
-$db_host['CIRCUITO'] = '(local)\SQL2012';
-$db_user['CIRCUITO'] = 'App_ModulosSAO';
-$db_pwd['CIRCUITO']  = '@msApp85%';
-$db_name['CIRCUITO'] = 'SAO1814_CIRCUITO';
-$db_type['CIRCUITO'] = 'OBRA';
+$db_host['SAO1814_CIRCUITO'] = '(local)';
+$db_user['SAO1814_CIRCUITO'] = 'App_ModulosSAO';
+$db_pwd['SAO1814_CIRCUITO']  = '@msApp85%';
+$db_name['SAO1814_CIRCUITO'] = 'SAO1814_CIRCUITO';
+$db_type['SAO1814_CIRCUITO'] = 'OBRA';
 
 // $dbServer['SAO1814_SUB'] 	 = '172.20.74.3';
 // $dbUser['SAO1814_SUB']   	 = 'App_ModulosSAO';
